dea7033d917621118bd33a63b4f33adadb0a249f
lmontero18 <lmonterom879@gmail.com>
2024-12-11 15:51:23 -0600

    chore(campaign-contract): basic structure for the contract is implemented

---
+++ contracts/Campaign.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.28;
+
+contract HooslingCampaign {
+    
+    struct Campaign {
+        uint id;
+        address creator;
+        string title;
+        string description;
+        uint goal;
+        uint fundsRaised;
+        uint deadline;
+        bool withdrawn;
+    }
+
+    struct Contribution {
+        address donor;
+        uint amount;
+    }
+
+    mapping(uint => Campaign) public campaigns;
+    mapping(uint => Contribution[]) public contributions;
+
+    uint public nextCampaignId;
+    constructor () {}
+
+    event CampaignCreated(uint indexed id, address indexed creator, uint goal, uint deadline);
+    event DonationReceived(uint indexed campaignId, address indexed donor, uint amount);
+    event FundsWithdrawn(uint indexed campaignId, address indexed creator, uint amount);
+    event RefundIssued(uint indexed campaignId, address indexed donor, uint amount);
+    
+
+}
